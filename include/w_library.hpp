b761f7af23723a2b70b27c3de8a588a21925f499
user42 <elias.saci@gmail.com>
2022-03-06 00:35:26 +0100

    auto

--- include/w_library.hpp
+++ include/w_library.hpp
@@ -1,8 +1,30 @@
 #ifndef W_LIBRARY_HPP
 # define W_LIBRABRY_HPP
 
+#	include <sys/socket.h>
+#	include <sys/types.h>
+#	include <signal.h>
+#	include <stdio.h>
+#	include <stdlib.h>
+#	include <string.h>
+#	include <unistd.h>
+#	include <arpa/inet.h>
+#	include <stdarg.h>
+#	include <errno.h>
+#	include <fcntl.h>
+#	include <sys/time.h>
+#	include <sys/ioctl.h>
+#	include <netdb.h>
+
 #	include <iostream>
 #	include <string>
 #	include <fstream>
+
+#	define SERVER_PORT 18000
+#	define MAXLINE 4096
+#	define SA struct sockaddr
+#	define SA_IN struct sockaddr_in
+
+
 
 # endif
