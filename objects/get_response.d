96f713ef3f540c27de1d03fed7b0dcb58a836a12
user42 <elias.saci@gmail.com>
2022-03-17 13:41:48 +0100

    auto

--- objects/get_response.d
+++
@@ -1,4 +0,0 @@
-objects/get_response.o: srcs/get_response.cpp \
- srcs/../include/w_library.hpp srcs/../include/w_defines.hpp \
- srcs/../include/p_conf.hpp srcs/../include/ClassParsingClientRequest.hpp \
- srcs/../include/server_data.hpp
