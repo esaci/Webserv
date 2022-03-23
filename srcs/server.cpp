d1bf4f273e99d7eff3cae0b59cf8babf004c3435
Elias <eliasnklo@gmail.com>
2022-03-23 10:04:57 +0100

    auto

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -28,7 +28,7 @@ int	init_socket( void ){
 int	server_data::_server( void ){
 	int		n;
 	size_t	pos, len;
-
+	// int	fd = open(TMPFAVICO, O_RDONLY);
 	serverfd = init_socket();
 	if (serverfd < 0)
 		return (1);
@@ -38,6 +38,13 @@ int	server_data::_server( void ){
 		len = tab_poll.size();
 		
 		// std::cout <<"Actuellement " << len - 1 << " client(s)" << std::endl;
+		// if (listening)
+		// {
+			// client_poll.fd = fd;
+			// client_poll.events = POLLIN;
+			// client_poll.revents = 0;
+			// tab_poll.push_back(client_poll);
+		// }
 		if ( !(n = poll(tab_poll.begin().base(), tab_poll.size(), TIMEOUT)) )
 			return (print_return("TIMEOUT: poll", 1));
 		if (n < 0)
@@ -45,6 +52,11 @@ int	server_data::_server( void ){
 		pos = 0;
 		for (std::vector<struct pollfd>::iterator it = tab_poll.begin(); it < tab_poll.end() && pos < len; ++pos, it = tab_poll.begin() + pos)
 		{
+			// if (listening && it->fd == fd && it->revents & POLLIN)
+			// {
+				// std::cerr << "CA FONCTIONNE \n\n";
+				// continue;
+			// }
 			if (listening && it->revents && !(it->revents & POLLIN) && !(it->revents & POLLOUT))
 				return (print_return("Revents chelou\n\n\n", 1));
 			if (setup_listening(it->fd))
