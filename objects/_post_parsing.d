795e8446cb1f8051abd9dfbe2dc380e7a1790c69
Elias <eliasnklo@gmail.com>
2022-03-25 17:35:52 +0100

    auto

--- objects/_post_parsing.d
+++
@@ -1,5 +0,0 @@
-objects/_post_parsing.o: srcs/_post_parsing.cpp \
-  srcs/../include/w_library.hpp srcs/../include/w_defines.hpp \
-  srcs/../include/p_conf.hpp \
-  srcs/../include/ClassParsingClientRequest.hpp \
-  srcs/../include/server_data.hpp
