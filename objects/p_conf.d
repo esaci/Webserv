be4d4eb81d926a0e4a962e2ce2699f5152de6357
Elias <eliasnklo@gmail.com>
2022-03-28 18:13:16 +0200

    auto

--- objects/p_conf.d
+++
@@ -1,4 +0,0 @@
-objects/p_conf.o: srcs/p_conf.cpp srcs/../include/w_library.hpp \
- srcs/../include/w_defines.hpp srcs/../include/p_conf.hpp \
- srcs/../include/ClassParsingClientRequest.hpp \
- srcs/../include/server_data.hpp
--- objects/server_data.d
+++
@@ -1,4 +0,0 @@
-objects/server_data.o: srcs/server_data.cpp srcs/../include/w_library.hpp \
- srcs/../include/w_defines.hpp srcs/../include/p_conf.hpp \
- srcs/../include/ClassParsingClientRequest.hpp \
- srcs/../include/server_data.hpp
