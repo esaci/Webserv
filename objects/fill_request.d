76fff41399d04fdf35ab1dddc279d4bc576e308d
user42 <elias.saci@gmail.com>
2022-03-28 21:58:16 +0200

    auto

--- objects/fill_request.d
+++
@@ -1,4 +0,0 @@
-objects/fill_request.o: srcs/fill_request.cpp \
- srcs/../include/w_library.hpp srcs/../include/w_defines.hpp \
- srcs/../include/p_conf.hpp srcs/../include/ClassParsingClientRequest.hpp \
- srcs/../include/server_data.hpp
--- objects/server_read.d
+++
@@ -1,4 +0,0 @@
-objects/server_read.o: srcs/server_read.cpp srcs/../include/w_library.hpp \
- srcs/../include/w_defines.hpp srcs/../include/p_conf.hpp \
- srcs/../include/ClassParsingClientRequest.hpp \
- srcs/../include/server_data.hpp
