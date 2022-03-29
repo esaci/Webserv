2e7198f11892eb95988a31f9638df32c3d31cfc0
osgens <ogenser@student.42.fr>
2022-03-29 19:46:44 +0200

    base

--- srcs/cgi.cpp
+++ srcs/cgi.cpp
@@ -4,7 +4,8 @@
 #include <unistd.h>
 
 int basic_cgi(void){
-    std::string cmd = "php-cgi";
-    char *args[3] = {"php-cgi", "'echo \"Hello World\n\";'\";", NULL};
-    execve(cmd, args, NULL);
+    // std::string cmd = "php-cgi";
+    // char *args[3] = {"php-cgi", "'echo \"Hello World\n\";'\";", NULL};
+    // execve(cmd, args, NULL);
+    return 0;
 }
