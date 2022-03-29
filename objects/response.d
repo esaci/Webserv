182333f8b18877d3db71c8faa470a37601827682
osgens <ogenser@student.42.fr>
2022-03-29 06:23:26 +0200

    test

--- objects/response.d
+++
@@ -1,4 +0,0 @@
-objects/response.o: srcs/response.cpp srcs/../include/w_library.hpp \
-  srcs/../include/w_defines.hpp srcs/../include/p_conf.hpp \
-  srcs/../include/ClassParsingClientRequest.hpp \
-  srcs/../include/server_data.hpp
--- srcs/_post_parsing.cpp
+++ srcs/_post_parsing.cpp
@@ -18,20 +18,23 @@ size_t		RP15::extract_body_check( void ){
 }
 
 int			_post_read_cl(std::vector<pollfd>::iterator it){
-		recvline.clear();
-	if ((n = recv(it->fd, recvline.begin().base(), MAXLINE, 0)) > 0)
-		tab_request[it->fd].parse_data.insert(tab_request[it->fd].parse_data.end(), recvline.begin().base(), recvline.begin().base() + n);
+	(void)it;
+	return 0;
+		// recvline.clear();
+	// if ((n = recv(it->fd, recvline.begin().base(), MAXLINE, 0)) > 0)
+		// tab_request[it->fd].parse_data.insert(tab_request[it->fd].parse_data.end(), recvline.begin().base(), recvline.begin().base() + n);
 }
 
 int			_post_read_ch(std::vector<pollfd>::iterator it){
-	
+	(void)it;
+	return 0;
 }
 
 int			server_data::_post_server_read(std::vector<pollfd>::iterator it){
-	int n;
+	// int n;
 
-	if (tab_request[it->fd].content_length.size())
-		return (_post_read_cl(it));
+	// if (tab_request[it->fd].content_length.size())
+	// 	return (_post_read_cl(it));
 	return (_post_read_ch(it));
 }
 
