50fcc59aa759407e16ac5f627e337b952c88fb3e
osgens <ogenser@student.42.fr>
2022-04-04 23:58:25 +0200

    end of day

--- srcs/cgi.cpp
+++ srcs/cgi.cpp
@@ -12,6 +12,7 @@
 
 void    RP15::set_cgi_env(void){
     std::vector<char *> env;
+    std::string cgipath("/usr/bin/php-cgi");                // mettre a jour avec parsing raph
     std::string content_lengthenv(this->content_length.begin(), this->content_length.end());
     std::string hostenv(this->host.begin(), this->host.end());
     std::string acceptenv(this->accept.begin(), this->accept.end());
@@ -20,18 +21,9 @@ void    RP15::set_cgi_env(void){
     std::string methodenv(this->method.begin(), this->method.end());
     
     env.push_back((char *)"SERVER_SOFTWARE=webserv1.0");
-    env.push_back((char*)"GATEWAY_INTERFACE=PHP 7.2.24-0ubuntu0.18.04.11");
+    env.push_back((char*)"GATEWAY_INTERFACE=CGI/1.1");
     env.push_back((char*)"SERVER_PROTOCOL=HTTP/1.1");
     env.push_back((char*)"REDIRECT_STATUS0=200");
-  
-    // std::string metenv("REDIRECT_STATUS0=");
-    // metenv += methodenv;
-    // // env.push_back((char*)("REDIRECT_STATUS0=200" += methodenv));
-    // std::cout << metenv << "holq\n";
-    // env.push_back((char *)metenv.c_str());
-
-
-
     std::string content_lengthv("CONTENT_LENGTH=");
     content_lengthv += content_lengthenv;
     env.push_back((char *)content_lengthv.c_str());
@@ -50,30 +42,33 @@ void    RP15::set_cgi_env(void){
     std::string methodv("REQUEST_METHOD=");
     methodv += methodenv;
     env.push_back((char *)methodv.c_str());
+    std::string path_infov("PATH_INFO=");
+    path_infov += cgipath;
+    env.push_back((char *)path_infov.c_str());
+    std::string path_translatedv("PATH_TRANSLATED=");
+    path_translatedv += cgipath;
+    env.push_back((char *)path_translatedv.c_str());
+    std::string script_namev("SCRIPT_NAME=");
+    script_namev += cgipath;
+    env.push_back((char *)script_namev.c_str());
+    std::string query_stringv("QUERY_STRING=");
+    // query_stringv += ;                               // var par elias
+    env.push_back((char *)query_stringv.c_str());
+    std::string content_typev("CONTENT_TYPE=");
+    // content_typev += ;                                // var par elias
+    env.push_back((char *)content_typev.c_str());
 
+    // for(std::vector<char *>::iterator it = env.begin() ;it != env.end(); it++)
+    //     std::cout << *it << "\n";
 
+    char *ev[15];
+    int i = 0;
+    for(std::vector<char *>::iterator it = env.begin() ;it != env.end(); it++, i++){
+        ev[i] = *it;
+         std::cout << ev[i] << "\n";
+    }
 
-    //A FINIR IL EN MAQUE COMMENTE EN DESSOUS
-
-    // env["SERVER_NAME="] = hostenv;   //remove :port at end : host name of the server, may be dot-decimal IP address.
-    // env["CONTENT_LENGTH="] = content_lengthenv;         
-    // env["HTTP_ACCEPT="] = acceptenv;
-    // env["HTTP_ACCEPT_LANGUAGE="] = accept_languageenv;  
-    // env["HTTP_USER_AGENT="] = user_agentenv;
-
-    // // env["QUERY_STRING="] =   // tout apres le points d'iterrogation
-    // // env["CONTENT_TYPE="] =   //type in header 
-
-    // env["REQUEST_METHOD="] = methodenv;
-
-    // //a mettre a jour avec le parsing de raph
-    // env["PATH_INFO="] = "/usr/bin/php-cgi";          // path to .php 
-    // env["PATH_TRANSLATED="] = "/usr/bin/php-cgi";      // le meme en path absolu
-    // env["SCRIPT_NAME="] = "/usr/bin/php-cgi";      //path vers .php : raph 
-
-    for(std::vector<char *>::iterator it = env.begin() ;it != env.end(); it++)
-        std::cout << *it << "\n";
-
+    // std::cout << &ev[0];
     /* optionnal:
     // env.["SERVER_PORT="] =   // a remplir parse port from host          : TCP port (decimal).
     // env.["REMOTE_HOST="] =               
