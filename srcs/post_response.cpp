65774538d4548024bcdab4ecc2fe135bb098ec44
Raphael KHELIF <rkhelif@e3r1p3.clusters.42paris.fr>
2022-04-09 13:11:56 +0200

    res

--- srcs/post_response.cpp
+++ srcs/post_response.cpp
@@ -27,6 +27,22 @@ int		RP15::_post_cgi(server_data *d_s, int clientfd){
 	return (d_s->_send(clientfd, 200));
 }
 
+bool check_bad_inf(DATA &ct, DATA &buff)
+{
+	std::string	v1("multipart/form-data;");
+	std::string	v2;
+	(void)buff;
+	//std::string v3;
+	if (ct.size() < 20) {return (1);}
+	for(DATA::iterator it = ct.begin(); (it != ct.end() && it != (ct.begin() + 20)); it++)
+		v2.push_back(*it);
+	if (v1 != v2) {return (1);}
+	/*for (DATA:iterator it = buff.begin(); (it != buff.end() && *it != '\n'); it++)
+		v3.push_back(*it);*/
+	
+	return (0);
+}
+
 void	parse_body_buff(DATA &buff)
 {
 	std::cout << buff << std::endl;
@@ -38,6 +54,8 @@ int	server_data::_post_upload(int clientfd)
 	std::string root = tab_tab_ap[sockets_to_hosts[tab_request[clientfd].serverfd]][0].get_root((char*)tab_request[clientfd].ressource.begin().base());
 	std::cout << root << std::endl;
 	std::cout << "-----------------raph----------------------" << std::endl;
+	check_bad_inf(tab_request[clientfd].content_type, tab_request[clientfd].r_body_buffer);
+	std::cout << "raph:" << tab_request[clientfd].content_type << std::endl;
 	parse_body_buff(tab_request[clientfd].r_body_buffer);
 	std::cout << "-----------------raph----------------------" << std::endl;
 	// TOUTE LES INFOS DU CLIENT SONT SUR tab_request[clientfd]
