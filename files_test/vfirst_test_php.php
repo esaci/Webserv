945948c15af4c788e1ceed1bad911be52287fb8a
osgens <ogenser@student.42.fr>
2022-04-09 21:37:47 +0200

    test elias

--- files_test/vfirst_test_php.php
+++ files_test/vfirst_test_php.php
@@ -1,8 +1,16 @@
-<?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+  <head>
+    <title>Ca marche !</title>
+  </head>
+  <body>
+  <?php
+  echo '<strong>LE CGI FONCTIONNE  !</strong>';
   $rp15 = htmlspecialchars($_POST['rp15']);
   $brp15  = htmlspecialchars($_POST['brp15']);
 
   echo  'Tu penses donc que Raphifou15 est forcement ', $rp15, ' , Ah bon ??? ', $brp15;
 ?>
+  </body>
+</html>
